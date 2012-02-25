c6853fee1e1e5fed28fcebccd3e7ab3ca9ef41ec
Erik Stainsby <erik.stainsby@roaringsky.ca>
2012-02-25 14:17:26 -0800

    Refactor is about to begin

--- ReactionAddClass/ReactionAddClass.m
+++ ReactionAddClass/ReactionAddClass.m
@@ -25,6 +25,10 @@
 	return YES;
 }
 
+- (NSString *) reactionBehaviour {
+	return [NSString stringWithFormat:@".addClass('%@')",[addClassField stringValue]];
+}
+
 - (NSString *) callbackFunction {
 	
 	NSString * myFunc = @"";
@@ -34,7 +38,7 @@
 	else {
 		myFunc = [myFunc stringByAppendingFormat:@"$('%@')", [[self selectorField] stringValue]];
 	}
-	myFunc = [myFunc stringByAppendingFormat:@".addClass('%@')",[addClassField stringValue]];
+	myFunc = [myFunc stringByAppendingString: [self reactionBehaviour]];
 	
 	return [NSString stringWithFormat:@"function(event,elem){%@}", myFunc];
 }
