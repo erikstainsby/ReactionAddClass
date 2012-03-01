152659169d1a03f81383874b165c338ea1e23009
Erik Stainsby <erik.stainsby@roaringsky.ca>
2012-03-01 11:59:52 -0800

    Refactored name from ReactionAddClass to orthogonal  ReactionClassAdd

---
+++ ReactionAddClass/ReactionClassAdd.h
@@ -0,0 +1,14 @@
+//
+//  ReactionAddClass.h
+//  ReactionAddClass
+//
+//  Created by Erik Stainsby on 12-02-18.
+//  Copyright (c) 2012 Roaring Sky. All rights reserved.
+//
+
+#import <RSTrixiePlugin/RSReactionPlugin.h>
+
+@interface ReactionClassAdd : RSReactionPlugin
+
+
+@end
---
+++ ReactionAddClass/ReactionClassAdd.m
@@ -0,0 +1,28 @@
+//
+//  ReactionAddClass.m
+//  ReactionAddClass
+//
+//  Created by Erik Stainsby on 12-02-18.
+//  Copyright (c) 2012 Roaring Sky. All rights reserved.
+//
+
+#import "ReactionClassAdd.h"
+
+@implementation ReactionClassAdd
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+	
+    if( nil != (self = [super initWithNibName:nibNameOrNil	bundle:nibBundleOrNil]))
+    {
+		[self setPluginName: @"Add Class"];
+		[self setAction:@"addClass"];
+    }
+    return self;
+} 
+
+- (NSString *) callback {
+	return [NSString stringWithFormat:@"$('%@').%@('%@');",[[self targetField] stringValue],[self action],[[self deltaField] stringValue]];
+}
+
+
+@end
